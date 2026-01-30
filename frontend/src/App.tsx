af3fd87a60d9681dbd0d6fcaad106f4ea3ec0892
Hevanafa <dhevansafa4@gmail.com>
2026-01-30 10:50:37 +0700

    added fetch post prototype

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import axios from "axios";
 import "./App.scss";
 
 function App() {
   const [urlInput, setUrlInput] = useState("");
+  const [isDownloading, setIsDownloading] = useState(false);
+
   const [thumbnails, setThumbnails] = useState([]);
 
   // function keydownHandler(e: KeyboardEventHandler<HTMLInputElement>) {
@@ -15,11 +18,23 @@ function App() {
         <input
           type="text"
           value={urlInput}
-          
+
           onChange={e => { setUrlInput(e.target.value) }}
-          onKeyDown={e => {
-            if (e.key == "Enter")
-              console.log(urlInput);
+          onKeyDown={async e => {
+            if (e.key == "Enter") {
+              if (isDownloading) return;
+
+              setIsDownloading(true);
+
+              const postBody = {
+                url: urlInput
+              };
+
+              const response = await axios.post("http://localhost:8001", postBody);
+              console.log(response.data);
+
+              setIsDownloading(false);
+            }
             }} />
       </div>
 
