35d81bbd9b0aea6e583096414638913951466868
Hevanafa <dhevansafa4@gmail.com>
2026-01-30 10:55:17 +0700

    added CORS dependency

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const Port = 8001;
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
