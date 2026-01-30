2d0c6881b6c0d7ca46782f13aae44311e13557bc
Hevanafa <dhevansafa4@gmail.com>
2026-01-30 10:44:39 +0700

    added server prototype

---
+++ backend/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const Port = 8001;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Bun + Express!" })
+});
+
+app.listen(Port, () => {
+  console.log("Server running on localhost:" + Port);
+});
+
